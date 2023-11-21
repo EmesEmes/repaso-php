4a4a323d9691d4cc1c5ce031431faa32cf16a40b
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-21 10:55:01 -0500

    Atributos y constructores repasado

--- 03.php
+++ 03.php
@@ -1,2 +1,22 @@
 <?php
 include 'includes/header.php';
+
+class Empleado {
+    public $nombre;
+    public $apellido;
+    public $departamento;
+    public $email;
+    public $codigo;
+}
+
+$empleado = new Empleado;
+
+$empleado->nombre = "Juan";
+$empleado->apellido = "Perez";
+$empleado->departamento = "Ventas";
+$empleado->email = "correo@correo.com";
+$empleado->codigo = 1234;
+
+echo "<pre>";
+var_dump($empleado);
+echo "</pre>";
--- 04.php
+++ 04.php
@@ -1,2 +1,44 @@
 <?php
 include 'includes/header.php';
+
+class Empleado {
+    public $nombre;
+    public $apellido;
+    public $departamento;
+    public $email;
+    public $codigo;
+
+    public function __construct($nombre, $apellido, $departamento, $email, $codigo)
+    {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->departamento = $departamento;
+        $this->email = $email;
+        $this->codigo = $codigo;
+    }
+    
+}
+
+$empleado = new Empleado("Juan", "Perez", "Ventas", "correo@correo.com", 1234);
+
+//segunda instancia
+$empleado2 = new Empleado("Mimi", "Herrera", "Sistemas", "correo3@correo.com", 3333);
+
+
+//Reasignar Valores
+
+$empleado->nombre = "Emilio";
+$empleado->apellido = "del Hierro";
+$empleado->departamento = "Sistemas";
+$empleado->email = "correo2@correo.com";
+$empleado->codigo = 333111;
+
+
+
+echo "<pre>";
+var_dump($empleado);
+echo "</pre>";
+
+echo "<pre>";
+var_dump($empleado2);
+echo "</pre>";

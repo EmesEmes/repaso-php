cf1c6f82acd81c0508dca70afc63ac1fd527857b
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-21 11:05:04 -0500

    Constructor property promotion repasado

--- 05.php
+++ 05.php
@@ -1,2 +1,40 @@
 <?php
 include 'includes/header.php';
+
+//Forzar tipado
+
+class Empleado {
+    public $nombre;
+    public $apellido;
+    public $departamento;
+    public $email;
+    public $codigo;
+
+    public function __construct(string $nombre, string $apellido, string $departamento, string $email, int $codigo)
+    {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->departamento = $departamento;
+        $this->email = $email;
+        $this->codigo = $codigo;
+    }
+    
+}
+
+$empleado = new Empleado("Juan", "Perez", "Ventas", "correo@correo.com", 1234);
+
+//segunda instancia
+$empleado2 = new Empleado("Mimi", "Herrera", "Sistemas", "correo3@correo.com", 3333);
+
+
+
+
+
+
+echo "<pre>";
+var_dump($empleado);
+echo "</pre>";
+
+echo "<pre>";
+var_dump($empleado2);
+echo "</pre>";
--- 06.php
+++ 06.php
@@ -1,2 +1,29 @@
 <?php
 include 'includes/header.php';
+
+// Constructor Properties Promotion
+
+class Empleado {
+
+    public function __construct(
+        public $nombre,
+        public $apellido,
+        public $departamento,
+        public $email,
+        public $codigo,
+    ){}
+    
+}
+
+$empleado = new Empleado("Juan", "Perez", "Ventas", "correo@correo.com", 1234);
+
+//segunda instancia
+$empleado2 = new Empleado("Mimi", "Herrera", "Sistemas", "correo3@correo.com", 3333);
+
+echo "<pre>";
+var_dump($empleado);
+echo "</pre>";
+
+echo "<pre>";
+var_dump($empleado2);
+echo "</pre>";

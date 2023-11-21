62454b5e15fa1c5f9da9aee259666ed5ff7441e5
Emilio del Hierro <delhierroemilio@gmail.com>
2023-11-21 13:05:46 -0500

    Metodos estaticos repasado

--- 12.php
+++ 12.php
@@ -1,2 +1,48 @@
 <?php
 include 'includes/header.php';
+
+// Metodos estaticos
+// No se necesita instanciar la clase para poder acceder a sus metodos
+// en este caso se usa self:: para acceder a los atributos estaticos
+class Empleado {
+   
+
+    protected static $nombre;
+    protected $apellido;
+    public $departamento;
+    public $email;
+    public $codigo;
+
+    public function __construct($nombre, $apellido, $departamento, $email, $codigo)
+    {
+        self::$nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->departamento = $departamento;
+        $this->email = $email;
+        $this->codigo = $codigo;
+    }
+    public function obtenerNombre() {
+        return $this->nombre . "<br>";
+    }
+
+    public function cambiarNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public static function obtenerEmpleado() {
+        echo "desde metodo estatico" . "<br>";
+    }
+    
+    public static function obtenerNombreStatic() {
+        return self::$nombre . "<br>";
+    }
+}
+
+Empleado::obtenerEmpleado();
+
+$empleado = new Empleado("Juan", "Perez", "Ventas", "empleado@empresa", 1234); 
+echo $empleado::obtenerNombreStatic(); 
+
+echo "<pre>";
+var_dump($empleado);
+echo "</pre>";
